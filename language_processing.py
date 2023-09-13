b573aba5c65427c88ccf558e2a017148c38a3a40
kuna <zcanepa19@gmail.com>
2023-09-12 21:26:54 -0400

    Added algorithms and metrics for future reference

---
+++ language_processing.py
@@ -0,0 +1,11 @@
+''''''
+# Choose a package from the ones below:
+# import nltk
+# import spacy
+# import gensim
+# import textblob
+# import polyglot
+# import flair
+# import fasttext
+# import transformers
+# import stanfordnlp
